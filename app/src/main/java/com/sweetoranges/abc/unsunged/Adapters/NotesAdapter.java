4b20af0943a5fd0cb483b98ee763d129b007006a
Rushabh <rushabh.insomniacs@gmail.com>
2019-04-20 17:24:00 +0530

    ip

--- app/src/main/java/com/sweetoranges/abc/unsunged/Adapters/NotesAdapter.java
+++ app/src/main/java/com/sweetoranges/abc/unsunged/Adapters/NotesAdapter.java
@@ -1,23 +1,22 @@
 package com.sweetoranges.abc.unsunged.Adapters;
 
 import android.content.Context;
-import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import com.sweetoranges.abc.unsunged.R;
-import com.sweetoranges.abc.unsunged.Sqlite.Note;
+import com.sweetoranges.abc.unsunged.Sqlite.Shop;
+
 import java.util.List;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder> {
 
     public Context context;
-    public List<Note> notesList;
+    public List<Shop> notesList;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView note;
@@ -30,10 +29,9 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder
         }
     }
 
-    public NotesAdapter(Context context, List<Note> notesList) {
+    public NotesAdapter(Context context, List<Shop> notesList) {
         this.context = context;
         this.notesList = notesList;
-        Toast.makeText(context, notesList.get(0).getNote(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -45,7 +43,7 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        holder.note.setText(notesList.get(position).getNote());
+        holder.note.setText(notesList.get(position).getName());
         holder.cross.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/sweetoranges/abc/unsunged/SearchFragment/SearchFragment.java
+++ app/src/main/java/com/sweetoranges/abc/unsunged/SearchFragment/SearchFragment.java
@@ -1,40 +1,35 @@
 package com.sweetoranges.abc.unsunged.SearchFragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.sweetoranges.abc.unsunged.Adapters.MusicTypeAdapter;
 import com.sweetoranges.abc.unsunged.Adapters.NotesAdapter;
 import com.sweetoranges.abc.unsunged.Adapters.SearchAdapter;
-import com.sweetoranges.abc.unsunged.Classes.OnBackPressed;
 import com.sweetoranges.abc.unsunged.R;
-import com.sweetoranges.abc.unsunged.Sqlite.DatabaseHelper;
-import com.sweetoranges.abc.unsunged.Sqlite.Note;
+import com.sweetoranges.abc.unsunged.Sqlite.DBHandler;
+import com.sweetoranges.abc.unsunged.Sqlite.Shop;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SearchFragment extends Fragment  {
     public RecyclerView searchRecycler,search,typeRecyc;
     public AppCompatTextView Profile,Type,Language,Mood;
     EditText edtsearch;
     Button btn;
-    public List<Note> notesList = new ArrayList<>();
-    public DatabaseHelper db = new DatabaseHelper(getActivity());
 
+    List<Shop> shops = new ArrayList<>();
     public String name[] = new String[]{};
     public String lang[] = new String[]{"English","Hindi","Gujrati","Rajasthani"};
     public String mood[] = new String[]{"Soothing","Travelling","Happy","Nostalgia","Inspirational","Slow"};
@@ -43,6 +38,7 @@ public class SearchFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_search, container, false);
+        DBHandler db=new DBHandler(getActivity());
         searchRecycler=(RecyclerView)view.findViewById(R.id.searchRecycler);
         search=(RecyclerView)view.findViewById(R.id.search);
         btn=(Button)view.findViewById(R.id.btn);
@@ -56,6 +52,11 @@ public class SearchFragment extends Fragment  {
         typeRecyc.setAdapter(new MusicTypeAdapter(getActivity(),name));
         searchRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, true));
         searchRecycler.setAdapter(new SearchAdapter(getActivity()));
+
+       // shops=db.getAllShops();
+       // Toast.makeText(getActivity(), new DBHandler(getActivity()).getShop(0).getName(), Toast.LENGTH_SHORT).show();
+        search.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, true));
+     //   search.setAdapter(new NotesAdapter(getActivity(), shops));
 
         Profile.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
@@ -75,19 +76,11 @@ public class SearchFragment extends Fragment  {
             }});
         edtsearch.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
-//        try{notesList.addAll(db.getAllNotes());    maybe issue is ghere in addall
-//             Toast.makeText(getActivity(), notesList.get(0).getNote(), Toast.LENGTH_SHORT).show();
-//
-//        }catch (Exception e){
-//            Toast.makeText(getActivity(), "hrere", Toast.LENGTH_SHORT).show();
-//        }
-                search.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, true));
-                //  search.setAdapter(new NotesAdapter(getActivity(),notesList));
-            }});
+
+        }});
         btn.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
-                createNote(edtsearch.getText().toString());
-
+              //  db.addShop(new Shop(edtsearch.getText().toString()));
             }});
 //        edtsearch.setOnKeyListener(new View.OnKeyListener() {
 //            public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -100,17 +93,11 @@ public class SearchFragment extends Fragment  {
 //                return false;
 //            }
 //        });
+
         return view;
     }
 
-    private void createNote(String note) {
-            long id = db.insertNote(note);
-        Note n = db.getNote(id);
-        if (n != null) {
-            notesList.add(0, n);
-          //  mAdapter.notifyDataSetChanged();
-        }
-    }
+
 }
 // try {
 //
